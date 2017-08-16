79f268267b5dac9564d0f0f397acb396884469db
goodluckyboy <1354673776@qq.com>
2017-08-16 15:36:09 +0800

    加包

---
+++ src/cn/com/controller/UserInfoController.java
@@ -0,0 +1,5 @@
+package cn.com.controller;
+
+public class UserInfoController {
+
+}
---
+++ src/cn/com/service/impl/UserInfoService.java
@@ -0,0 +1,7 @@
+package cn.com.service.impl;
+
+import cn.com.service.IUserInfoService;
+
+public class UserInfoService implements IUserInfoService{
+
+}
